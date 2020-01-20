5f29025e533757abb6c86689ce8da629d9719982
Abdullah <abdullahchattha@gmail.com>
2020-01-20 17:06:17 -0500

    first commit

---
+++ Assets/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Apple : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public static float bottomY = -20f;
+
+	void Update () {
+		if ( transform.position.y < bottomY ) {
+			Destroy( this.gameObject );
+			// Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // b
+			// Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed() ;
+		}
+	}
+}
